07b7d71c757b10e7624a352e1df8f110b30a0afe
Farhan Masood <farhan.masood@mslm.io>
2024-01-20 12:18:01 +0500

    sidebar

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,14 +1,16 @@
-"use client"
+"use client";
 
 import { useConvexAuth } from "convex/react";
-import { useScrollTop } from "@/hooks/use-scroll-top"
-import { cn } from "@/lib/utils";
-import { Logo } from "./logo";
-import { ModeToggle } from "@/components/mode-toggle";
 import { SignInButton, UserButton } from "@clerk/clerk-react";
+import Link from "next/link";
+
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/spinner";
-import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+import { Logo } from "./logo";
 
 export const Navbar = () => {
     const { isAuthenticated, isLoading } = useConvexAuth();
@@ -16,7 +18,8 @@ export const Navbar = () => {
 
     return (
         <div className={cn(
-            "z-50 bg-background dark:bg-[#1f1f1f] fixed top-0 flex items-center w-full p-6", scrolled && 'border-b shadow-sm'
+            "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
+            scrolled && "border-b shadow-sm"
         )}>
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
@@ -26,21 +29,27 @@ export const Navbar = () => {
                 {!isAuthenticated && !isLoading && (
                     <>
                         <SignInButton mode="modal">
-                            <Button variant="ghost" size="sm">Log in</Button>
+                            <Button variant="ghost" size="sm">
+                                Log in
+                            </Button>
                         </SignInButton>
                         <SignInButton mode="modal">
-                            <Button size="sm">Get Jotion Free</Button>
+                            <Button size="sm">
+                                Get Jotion free
+                            </Button>
                         </SignInButton>
                     </>
                 )}
-                {isAuthenticated && (
+                {isAuthenticated && !isLoading && (
                     <>
                         <Button variant="ghost" size="sm" asChild>
                             <Link href="/documents">
                                 Enter Jotion
                             </Link>
                         </Button>
-                        <UserButton afterSignOutUrl="/" />
+                        <UserButton
+                            afterSignOutUrl="/"
+                        />
                     </>
                 )}
                 <ModeToggle />
